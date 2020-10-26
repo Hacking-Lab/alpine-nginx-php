66e80e10607d19b948b419a20095fabca4b8427e
Ivan Buetler <ivan.buetler@compass-security.com>
2020-10-26 15:27:21 +0100

    release 3.2

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t hackinglab/alpine-nginx-php -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-nginx-php:3.2.0 -t hackinglab/alpine-nginx-php:3.2 -t hackinglab/alpine-nginx-php:latest -f Dockerfile .
