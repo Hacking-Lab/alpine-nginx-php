e5ec23816294c5cbd8fe440169c01abc5649126e
Ivan Buetler <ivan.buetler@compass-security.com>
2022-07-29 09:55:14 +0200

    fix -8 in build script

--- build-release.sh
+++ build-release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 docker build --no-cache -t hackinglab/alpine-nginx-php-8:$1.0 -t hackinglab/alpine-nginx-php-8:$1 -t hackinglab/alpine-nginx-php-8:latest -f Dockerfile .
 
-docker push hackinglab/alpine-nginx-php
-docker push hackinglab/alpine-nginx-php:$1
-docker push hackinglab/alpine-nginx-php:$1.0
+docker push hackinglab/alpine-nginx-php-8
+docker push hackinglab/alpine-nginx-php-8:$1
+docker push hackinglab/alpine-nginx-php-8:$1.0
+
+
 
