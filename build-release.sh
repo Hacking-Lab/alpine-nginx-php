c8b9c1690cfe9ad29efb5226996294f119a56116
ibuetler <ivan.buetler@hacking-lab.com>
2024-06-18 16:54:49 +0200

    update

--- build-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-nginx-php-8:$1.0 -t hackinglab/alpine-nginx-php-8:$1 -t hackinglab/alpine-nginx-php-8:latest -f Dockerfile .
-
-docker push hackinglab/alpine-nginx-php-8
-docker push hackinglab/alpine-nginx-php-8:$1
-docker push hackinglab/alpine-nginx-php-8:$1.0
-
-
-
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-nginx-php-8:3.2.0 -t hackinglab/alpine-nginx-php-8:3.2 -t hackinglab/alpine-nginx-php-8:latest -f Dockerfile .
-
